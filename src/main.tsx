e62e156560dd6384e34f8c5be37117393da18ff9
FelipeZxz <caubraudo10@gmail.com>
2025-12-02 01:46:15 -0300

    Atualizar o main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,3 +3,13 @@ import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
+
+// === REGISTRO DO SERVICE WORKER PARA PWA ===
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then(() => console.log("Service Worker registrado com sucesso"))
+      .catch((err) => console.log("Erro ao registrar Service Worker:", err));
+  });
+}
